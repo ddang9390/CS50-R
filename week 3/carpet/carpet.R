e16aa781148db1757e80551d33f9a74ac9c03176
Daniel Dang <dnld1ng@yahoo.com>
2025-12-31 10:50:11 -0500

    Completed carpet problem

--- week 3/carpet/carpet.R
+++ week 3/carpet/carpet.R
@@ -1,9 +1,21 @@
 calculate_growth_rate <- function(years, visitors) {
-  # TODO: Calculate yearly growth of visitors
+  earliest_year <- min(years)
+  latest_year <- max(years)
+  
+  earliest_visitors <- visitors[1]
+  latest_visitors <- visitors[-1]
+  
+  growth_rate <- (latest_visitors - earliest_visitors) / (latest_year - earliest_year)
+  return(growth_rate)
 }
 
 predict_visitors <- function(years, visitors, year) {
-  # TODO: Predict visitors in given year
+  growth_rate <- calculate_growth_rate(years, visitors)
+  latest_visitors <- visitors[-1]
+  latest_year <- years[-1]
+  
+  prediction <- latest_visitors + (growth_rate * (year - latest_year))
+  return(prediction)
 }
 
 visitors <- read.csv("visitors.csv")
